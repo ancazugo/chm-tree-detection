9bb125744da6bf455b502833b56a3487b7501145
Andres Camilo Zuñiga-Gonzalez <ancazugo@hotmail.com>
2024-10-29 16:41:07 +0000

    Formatted paths for organization

--- chm_processing.R
+++ chm_processing.R
@@ -16,16 +16,18 @@ city_name <- 'London'
 # Data Paths --------------------------------------------------------------
 
 vom_dir <- here(RASTER_INPUT_DIR, "DEFRA", "VOM", city_name)
+vom_crowns_dir <- here(VECTOR_OUTPUT_DIR, "DEFRA", "crowns", city_name)
 
 tif_paths <- list.files(vom_dir, pattern = "\\.tif$", 
                         full.names = T, recursive = T)
 
-
-hillshade_paths <- tif_paths[str_detect(tif_paths, 'VOM_HS')]
+# hillshade_paths <- tif_paths[str_detect(tif_paths, 'VOM_HS')]
 chm_paths <- tif_paths[!str_detect(tif_paths, 'VOM_HS')]
 
-# TQ27nw/2075 from 2018 doesn't have a chm tile
-# TQ16se/1560 from 2018 is invalid format
+crowns_path <- here(vom_crowns_dir, paste0(city_name,'_crowns.gpkg'))
+
+# TQ27nw/2075 from 2018 doesn't have a chm tile (changed for 2021)
+# TQ16se/1560 from 2018 is invalid format (changed for 2021)
 
 # CHM Processing with lidR ------------------------------------------------
 
@@ -58,7 +60,7 @@ for (file in chm_paths) {
 }
 
 # Combine valid rasters into a single SpatRaster object
-chm_spat_rast <- merge(sprc(valid_rasters[42:45]))
+chm_spat_rast <- merge(sprc(valid_rasters))
 
 kernel <- matrix(1,3,3)
 chm_smoothed <- focal(chm_spat_rast, w = kernel, fun = median, na.rm = T)
@@ -68,7 +70,7 @@ chm_smoothed_rast <- raster::raster(chm_smoothed)
 terra::crs(chm_smoothed_rast) <- current_crs
 
 f <- function(x) {
-    y <- 2.6 * (-(exp(-0.08*(x-2)) - 1)) + 3
+    y <- 2.6 * (-(exp(-0.08 * (x - 2)) - 1)) + 3
     y[x < 2] <- 3
     y[x > 20] <- 5
     return(y)
@@ -87,4 +89,4 @@ names(crowns_vect) <- 'treeID'
 
 crowns_vect <- merge(crowns_vect, ttops_chm_smoothed_spat_vect, by = 'treeID')
 crowns_vect$area <- expanse(crowns_vect, unit = "m")
-writeVector(crowns_vect, 'crowns_func.gpkg', overwrite = T)
+writeVector(crowns_vect, crowns_path, overwrite = T)
